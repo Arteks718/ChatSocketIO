74a9601b00d5f2d616ea809db64ab442f470ebb3
Arteks718 <artexbro.official@gmail.com>
2023-08-19 09:13:36 +0300

    Update socket queries

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import name from './api'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { io } from "socket.io-client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// import name from './api'
 
+// socket.on('event', (payload) => {}); - подписка на событие
+// socket.emit('event', payload) - генерация события (на сервер)
 
+const socket = io("http://localhost:5000");
+socket.on("EVENT_FOR_ALL", (payload) => {
+  // console.log(payload);
+});
+socket.on("EVENT_FOR_SELF", (payload) => {
+  // console.log("payload for self:", payload);
+});
+socket.on("EVENT_FOR_OTHER", (payload) => {
+  // console.log("payload for other:", payload)
+})
+socket.emit('EVENT_FROM_SOCKET', 'Hello from client')
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+socket.on('SOME_EVENT_ON_SOME_SOCKET', (payload) => {
+  console.log('payload', payload)
+})
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
 
 reportWebVitals();
--- server/index.js
+++ server/index.js
@@ -8,12 +8,27 @@ const httpServer = http.createServer(() => {})
 //2
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://127.0.0.1:5000"
+    origin: "http://localhost:3000"
   }
 })
 
+// io.on('event', (payload) => {}) ~ btn.addEventListener('click', () => {})
+//    - подписка на событие
+// io.emit('event', payload) ~ dispatch(createUserThunk(newUser))
+//    - генерация события - для всех клиентов
+// socket.broadcast.emit('event', payload) -
+//    - генерация события - для всех, кроме себя
+
 io.on('connection', socket => {
-  io.emit('GLOBAL_EVENT', 'Hello from server')
+  console.log('Connection established')
+  io.emit('EVENT_FOR_ALL', 'Hello for everyone from server') // все видят
+  socket.emit('EVENT_FOR_SELF', 'Hello, you connection is established') // только тот кто подклюился
+  socket.broadcast.emit('EVENT_FOR_OTHER', 'New socket is connected') // все, кроме того что подключился
+
+  socket.on('EVENT_FROM_SOCKET', (payload) => {
+    console.log('payload :>>', payload)
+    socket.broadcast.emit('SOME_EVENT_ON_SOME_SOCKET', 'Event on some socket')
+  })
 })
 
 httpServer.listen(PORT, HOST, () => {
