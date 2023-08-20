d4abd4dcb1a2235e1ed1cd2894c58e7e6c516f25
Arteks718 <artexbro.official@gmail.com>
2023-08-20 11:26:43 +0300

    Add constants

--- server/socket.js
+++ server/socket.js
@@ -1,22 +1,26 @@
-const { Server } = require('socket.io') 
-const { Message } = require('./models')
+const { Server } = require("socket.io");
+const { Message } = require("./models");
+const {
+  SOCKET_EVENTS: { NEW_MESSAGE, NEW_MESSAGE_CREATED, NEW_MESSAGE_ERROR },
+} = require("./constants");
 
 const socketInit = (httpServer) => {
   const io = new Server(httpServer, {
-    cors: { origin: 'http://localhost:3000'},
-  })
-  io.on('connection', socket => {
-    console.log('socket is connected')
-    socket.on('NEW_MESSAGE', async payload => {
-      console.log('payload', payload)
+    cors: { origin: "http://localhost:3000" },
+  });
+  io.on("connection", (socket) => {
+    console.log("socket is connected");
+    const newMessageHandler = async (payload) => {
+      console.log("payload", payload);
       try {
-        const createdMessage = await Message.create(payload)
-        io.emit('NEW_MESSAGE_CREATED', createdMessage.toObject())
+        const createdMessage = await Message.create(payload);
+        io.emit(NEW_MESSAGE_CREATED, createdMessage.toObject());
       } catch (error) {
-        socket.emit('NEW_MESSAGE_ERROR', error)
+        socket.emit(NEW_MESSAGE_ERROR, error);
       }
-    })
-  })
-}
+    };
+    socket.on(NEW_MESSAGE, newMessageHandler);
+  });
+};
 
-module.exports = socketInit
+module.exports = socketInit;
