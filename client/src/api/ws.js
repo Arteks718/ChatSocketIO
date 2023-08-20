034e82557c330e6b5a325ac31aa65c53332a7155
Arteks718 <artexbro.official@gmail.com>
2023-08-20 11:05:47 +0300

    Add reducers for creating message

--- client/src/api/ws.js
+++ client/src/api/ws.js
@@ -1,16 +1,20 @@
-import {io} from 'socket.io-client'
+import { io } from "socket.io-client";
+import { createMessageError, createMessageFullfiled } from "../store/slices/messagesSlice";
 
-const socket = io('ws://localhost:5000')
+const socket = io("ws://localhost:5000");
 
-export const createMessageWs = payload => 
-  socket.emit('NEW_MESSAGE', payload)
+export const createMessageWs = (payload) => socket.emit("NEW_MESSAGE", payload);
 
-socket.on('NEW_MESSAGE_CREATED', payload => {
-  console.log('payload :>>' , payload)
-})
+export const bringStoreToWs = (store) => {
+  socket.on("NEW_MESSAGE_CREATED", (payload) => {
+    console.log("payload :>>", payload);
+    store.dispatch(createMessageFullfiled(payload))
+  });
 
-socket.on('NEW_MESSAGE_ERROR', payload => {
-  console.log('error :>>', payload)
-})
+  socket.on("NEW_MESSAGE_ERROR", (payload) => {
+    console.log("error :>>", payload);
+    store.dispatch(createMessageError(payload))
+  });
+};
 
 export default socket;
